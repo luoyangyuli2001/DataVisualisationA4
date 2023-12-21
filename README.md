fd13555e9d7883d9c7e4ba75da4093e8dbd8403b
Luo Yang <luoyangyuli2001@gmail.com>
2023-12-21 00:19:21 +0000

    Refactor the code.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+# COVID-19 Data Visualization App
+## Introduction 
+This application provides an interactive web interface for visualizing COVID-19 data, including trends in cases and deaths across various countries. It is built using Python with the Dash framework and Plotly for visualizations.
 
+## Installation
+### Clone the repository or download the source code
+First, clone the repository or download the source code to your local machine.
+### Install the required packages
+Install the required packages using the following command:
+```
+pip install -r requirements.txt
+```
+## Run the application
+### Start the app
+In the project directory, run the following command to start the application:
+```
+python app.py
+```
+### Access the application
+Once the app is running, open your web browser and visit `localhost:8050` to view the visualization dashboard.
--- app.py
+++ app.py
@@ -5,35 +5,35 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
-# 创建Dash应用
+# Create a Dash application
 app = dash.Dash(__name__)
 
 file_path = 'owid-covid-data.csv'
 
-# 读取数据集
+# Read the dataset
 covid_data = pd.read_csv(file_path)
 
-# 筛选时间范围：2021年1月1日至2021年12月31日
-covid_data['date'] = pd.to_datetime(covid_data['date'])  # 确保日期列是日期格式
+# Filter for the time range: January 1, 2021 to December 31, 2021
+covid_data['date'] = pd.to_datetime(covid_data['date'])  # Ensure the date column is in date format
 start_date = '2021-01-01'
 end_date = '2021-12-31'
 mask = (covid_data['date'] >= start_date) & (covid_data['date'] <= end_date)
 filtered_data = covid_data.loc[mask]
 
-# 选择需要的列：国家/地区代码，日期，新增数，确诊数，死亡数
+# Select the required columns: country code, date, new cases, total cases, deaths
 selected_columns = ['iso_code', 'location', 'date', 'new_cases', 'total_cases', 'new_deaths']
-processed_data = filtered_data[selected_columns].copy()  # 添加 .copy() 来创建副本
+processed_data = filtered_data[selected_columns].copy()  # Use .copy() to create a copy
 
-# 转换日期格式并排序
+# Convert date format and sort
 processed_data['date'] = pd.to_datetime(processed_data['date']).dt.strftime('%Y-%m-%d')
 processed_data.sort_values('date', inplace=True)
 
 countries = processed_data['location'].unique()
 country_options = [{'label': country, 'value': country} for country in countries]
 
-# Dash应用布局
+# Layout of the Dash application
 app.layout = html.Div([
-    # 单选按钮，用于选择数据类型
+    # Radio buttons to select data type
     dcc.RadioItems(
         id='data-type-selector',
         options=[
@@ -41,15 +41,15 @@ app.layout = html.Div([
             {'label': 'Deaths', 'value': 'new_deaths'},
             {'label': 'New cases', 'value': 'new_cases'}
         ],
-        value='total_cases'  # 默认值
+        value='total_cases'  # Default value
     ),
     dcc.Dropdown(
         id='country-selector',
         options=country_options,
-        value=['Ireland'],  # 将默认值设置为列表
-        multi=True  # 允许选择多个国家
+        value=['Ireland'],  # Set the default value as a list
+        multi=True  # Allow selection of multiple countries
     ),
-    # 用于显示地图的图表组件
+    # Chart component for displaying the map
     dcc.Graph(id='covid-map'),
     dcc.Graph(id='country-trend')
 ])
@@ -60,13 +60,13 @@ app.layout = html.Div([
      Input('country-selector', 'value')]
 )
 def update_map(selected_data_type, selected_countries):
-    # 如果没有国家被选中，则显示所有国家
+    # If no countries are selected, show all countries
     if selected_countries:
         filtered_data = processed_data[processed_data['location'].isin(selected_countries)]
     else:
         filtered_data = processed_data
 
-    # 创建地图
+    # Create a map
     fig = px.choropleth(
         filtered_data,
         locations="iso_code",
@@ -77,13 +77,14 @@ def update_map(selected_data_type, selected_countries):
         title=f"Data: {selected_data_type}",
     )
     return fig
+
 @app.callback(
     Output('country-trend', 'figure'),
     [Input('data-type-selector', 'value'),
      Input('country-selector', 'value')]
 )
 def update_country_trend(selected_data_type, selected_countries):
-    # 确保选择了一个且仅一个国家
+    # Ensure a single country is selected
     if selected_countries and len(selected_countries) == 1:
         country_data = processed_data[processed_data['location'] == selected_countries[0]]
         fig = px.line(
@@ -97,6 +98,6 @@ def update_country_trend(selected_data_type, selected_countries):
 
     return fig
 
-# 运行Dash应用
+# Run the Dash application
 if __name__ == '__main__':
     app.run_server(debug=True)
